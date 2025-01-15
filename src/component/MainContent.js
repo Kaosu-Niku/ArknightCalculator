679af73ae8dd895e8fb4eaf73a1785fd323bb13b
Kaosu-Niku <s9613010@gmail.com>
2025-01-16 01:41:25 +0800

    完成受天賦或職業特性影響面板數值、DPS、HPS的所有幹員的新計算結果以及附加上特殊符號標記

--- src/component/MainContent.js
+++ src/component/MainContent.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import MemberSpecial from '../model/MemberSpecial';
 
 function MainContent() {
   const [enemyHp, setEnemyHp] = useState(0);
@@ -8,13 +9,6 @@ function MainContent() {
   const [enemyRes, setEnemyRes] = useState(0);
   const [enemySpd, setEnemySpd] = useState(0);
   const [memberJsonData, setMemberJsonData] = useState([]);
-
-  // 提交敵人數據表單時執行
-  const submitEnemyForm = (event) => { 
-    event.preventDefault(); // 阻止表單提交行為
-    console.log('提交敵人數據表單成功');
-    console.log({ enemyHp, enemyAttackType, enemyAttack, enemyDef, enemyRes, enemySpd });
-  };
 
   // 我方數據的JSON檔案上傳時執行
   const uploadMemberData = (event) => {
@@ -36,36 +30,6 @@ function MainContent() {
 
   const memberDps = (row) => {
     let finalDamage = 0;
-    switch(row.attackType){
-      case "物傷":
-        finalDamage = row.attack - enemyDef;
-        if(finalDamage < row.attack / 20){
-          finalDamage = row.attack / 20;
-        }
-      return (Math.floor(finalDamage / row.spd));
-      case "法傷":
-        finalDamage = row.attack * ((100 - enemyRes) / 100);
-        if(finalDamage < row.attack / 20){
-          finalDamage = row.attack / 20;
-        }
-      return (Math.floor(finalDamage / row.spd));
-      default:
-      return 0;
-    }
-  }
-
-  const memberHps = (row) => {
-    switch(row.attackType){
-      case "治療":
-      return (Math.floor(row.attack / row.spd));
-      default:
-
-      return 0;
-    }
-  }
-
-  const memberKillTime = (row) => {
-    let finalDamage = 0;
     let finalDps = 0;
     switch(row.attackType){
       case "物傷":
@@ -73,17 +37,56 @@ function MainContent() {
         if(finalDamage < row.attack / 20){
           finalDamage = row.attack / 20;
         }
+        finalDps = (Math.floor(finalDamage / row.spd));        
+      return MemberSpecial.memberDpsSpecial(row, finalDps, {enemyHp, enemyAttackType, enemyAttack, enemyDef, enemyRes, enemySpd });
+      case "法傷":
+        finalDamage = row.attack * ((100 - enemyRes) / 100);
+        if(finalDamage < row.attack / 20){
+          finalDamage = row.attack / 20;
+        }
+        finalDps = (Math.floor(finalDamage / row.spd));
+      return MemberSpecial.memberDpsSpecial(row, finalDps, {enemyHp, enemyAttackType, enemyAttack, enemyDef, enemyRes, enemySpd });
+      default:
+      return 0;
+    }
+  }
+
+  const memberHps = (row) => {
+    let finalHps = 0;
+    switch(row.attackType){
+      case "治療":
+        finalHps = (Math.floor(row.attack / row.spd));
+      return MemberSpecial.memberHpsSpecial(row, finalHps);
+      default:
+      return 0;
+    }
+  }
+
+  const memberKillTime = (row) => {
+    let finalDamage = 0;
+    let finalDps = 0;
+    let trueDps = '';
+    let dpsStr = '';
+    switch(row.attackType){
+      case "物傷":
+        finalDamage = row.attack - enemyDef;
+        if(finalDamage < row.attack / 20){
+          finalDamage = row.attack / 20;
+        }
         finalDps = Math.floor(finalDamage / row.spd)
-      return (Math.ceil(enemyHp / finalDps));
+        trueDps = MemberSpecial.memberDpsSpecial(row, finalDps, {enemyHp, enemyAttackType, enemyAttack, enemyDef, enemyRes, enemySpd });
+        dpsStr = trueDps.replace(/\D/g, '');
+      return (Math.ceil(enemyHp / parseInt(dpsStr)));
       case "法傷":
         finalDamage = row.attack * ((100 - enemyRes) / 100);
         if(finalDamage < row.attack / 20){
           finalDamage = row.attack / 20;
         }
         finalDps = Math.floor(finalDamage / row.spd)
-      return (Math.ceil(enemyHp / finalDps));
+        trueDps = MemberSpecial.memberDpsSpecial(row, finalDps, {enemyHp, enemyAttackType, enemyAttack, enemyDef, enemyRes, enemySpd });
+        dpsStr = trueDps.replace(/\D/g, '');
+      return (Math.ceil(enemyHp / parseInt(dpsStr)));
       default:
-
       return "NO";
     }
   }
@@ -112,7 +115,7 @@ function MainContent() {
     <div className='container'>
       <div className='d-flex flex-column'>
         <div>     
-          <form id='enemy_form' onSubmit={submitEnemyForm}>
+          <form id='enemy_form'>
             <h1>敵人數據</h1>
             <label htmlFor="enemyHp">生命:</label>
             <input type="number" id="enemyHp" value={enemyHp} onChange={(e) => setEnemyHp(e.target.value)} min="0" required />
@@ -134,23 +137,31 @@ function MainContent() {
             <br></br>
             <label htmlFor="enemySpd">攻速:</label>
             <input type="number" id="enemySpd" value={enemySpd} onChange={(e) => setEnemySpd(e.target.value)} min="0" step="0.1" required />
-            <br></br> 
-            <button type="submit">計算</button>           
+            <br></br>        
           </form>          
         </div>
+        <hr></hr>
         <div>
           <h1>請選擇JSON檔:</h1>
           <input type="file" accept=".json" onChange={uploadMemberData} />
         </div>
-        
+        <hr></hr>
+        <p>以下我方數據皆以精1滿級滿潛能滿信賴及天賦加成為準</p>
+        <p>我方名稱帶有*表示其面板數值為經天賦加成後的最終結果</p>
+        <p>(ex: 波登可天賦為在場時輔助幹員加攻擊力，由於波登可自身就為輔助幹員，基本等同於永久加成，因此帶入計算)</p>
+        <p>我方DPS和HPS帶有*表示其數值為受職業特性或天賦影響後的最終結果</p>
+        <p>(ex: 酸糖天賦為至少造成20%傷害，因此刮痧時打出的保底傷害與正常幹員的5%不一樣需另外計算)</p>
+        <p>我方DPS和HPS帶有+表示其數值可能受職業特性或天賦影響，但由於是概率或必須滿足特定條件才觸發，因此不帶入計算，只計算無觸發的正常結果</p>
+        <p>(幹員頭像取自PRTS)</p>
         <table id="member_table" className="table table-bordered table-hover">
           <thead>
             <tr className='table-secondary'>
-              <th rowSpan='1' colSpan='11'>我方數據</th>
+              <th rowSpan='1' colSpan='12'>我方數據</th>
               <th rowSpan='1' colSpan='3'>敵人數據</th>
             </tr>
             <tr className='table-secondary'>
-                <th>幹員名稱</th>
+                <th></th>
+                <th>名稱</th>
                 <th>職業</th>
                 <th>生命</th>
                 <th>傷害類型</th>
@@ -167,6 +178,7 @@ function MainContent() {
           <tbody>
             {memberJsonData.map((row, index) => (
               <tr key={index}>
+                <td className="text-center"><img src={row.icon} alt='member_icon' width='50' height='50' /></td>
                 <td>{row.name}</td>
                 <td>{row.type}</td>
                 <td>{row.hp}</td>
---
+++ src/model/MemberSpecial.js
@@ -0,0 +1,85 @@
+const MemberSpecial = {
+  memberDpsSpecial: (row, originalDps, emenyData) => {
+    let finalDamage = 0;
+    switch(row.name){
+      case "紅豆":
+      return `${originalDps}+`;
+      case "慕斯":
+      return `${originalDps}+`;
+      case "宴":
+      return `${originalDps}+`;
+      case "獵蜂":
+      return `${originalDps}+`;
+      case "月見夜":
+      return `${originalDps}+`;
+      case "芳汀":
+      return `${originalDps}+`;
+      case "刻刀":
+        // 職業特性為攻擊皆是二連擊
+      return `${originalDps * 2}*`;
+      case "克洛絲":
+      return `${originalDps}+`;
+      case "流星":
+      return `${originalDps}+`;
+      case "躍躍":
+      return `${originalDps}+`;
+      case "酸糖":
+        // 天賦為至少保底20%傷害
+        finalDamage = 0;
+        finalDamage = row.attack - emenyData.enemyDef;
+        if(finalDamage < row.attack / 5){
+          finalDamage = row.attack / 5;
+        }
+      return `${(Math.floor(finalDamage / row.spd))}*`;
+      case "鉛踝":
+      return `${originalDps}+`;
+      case "松果":
+      return `${originalDps}+`;
+      case "夜煙":
+        // 天賦為攻擊給敵人-10%法抗1秒(夜煙自身攻擊都能吃到加成)
+        finalDamage = row.attack * ((100 - Math.ceil(emenyData.enemyRes * 0.9)) / 100);
+        if(finalDamage < row.attack / 20){
+          finalDamage = row.attack / 20;
+        }
+      return `${(Math.floor(finalDamage / row.spd))}*`;
+      case "卡達":
+      return `${originalDps}+`;
+      case "古米":
+      return `${originalDps}+`;
+      case "巫役小車":
+        // 天賦為部署後的40秒內每次攻擊附帶60凋亡損傷，且使攻擊範圍內所有敵人+10%法術脆弱和+10%元素脆弱
+        // (所以實際上應該是每次攻擊附帶66凋亡損傷，而小車40秒內可以打25下，所以造成凋亡損傷的總值為1650)        
+        finalDamage = row.attack * ((100 - emenyData.enemyRes) / 100) * 1.1;
+        if(finalDamage < row.attack / 20){
+          finalDamage = row.attack / 20;
+        }
+        // (普通與精英敵人的損傷累計值為1000，BOSS敵人的損傷累計值為2000)
+        // (對普通與精英敵人來說，小車可於第16下打爆條，也就是部屬後約25秒)
+        // (凋亡損傷爆條期間造成每秒800元素傷害，持續15秒，所以造成元素傷害的總傷為12000)
+      return `${(Math.floor(finalDamage / row.spd))}*`;
+      case "孑":
+      return `${originalDps}+`;
+      case "雲跡":
+      return `${originalDps}+`;
+      default:
+      return `${originalDps}`;
+    }
+  },
+  memberHpsSpecial: (row, originalHps) => {
+    switch(row.name){
+      case "安賽爾":
+      return `${originalHps}+`;
+      case "蘇蘇洛":
+      return `${originalHps}+`;
+      case "褐果":
+      return `${originalHps}+`;
+      case "調香師":
+        let otherHps = Math.floor(row.attack / 100 * 3);
+      return `${originalHps + otherHps}*`;
+      default:
+      return `${originalHps}`;
+    }
+  }
+}
+
+export default MemberSpecial;
